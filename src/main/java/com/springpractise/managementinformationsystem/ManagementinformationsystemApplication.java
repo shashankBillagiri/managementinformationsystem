57084b48eaaa7d590c0f155cbbabbf3c08a71953
shashank <shashankbillagiri93@gmail.com>
2023-03-03 19:49:53 -0600

    Initial commit

---
+++ src/main/java/com/springpractise/managementinformationsystem/ManagementinformationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.springpractise.managementinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementinformationsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementinformationsystemApplication.class, args);
+    }
+
+}
