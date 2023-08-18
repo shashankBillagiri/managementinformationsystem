a109b43601e9eb36fed6fd7cae92294c74a3ada7
shashank <shashankbillagiri93@gmail.com>
2023-08-17 21:05:38 -0500

    Added Post call

--- src/main/java/com/springpractise/managementinformationsystem/controller/StudentController.java
+++ src/main/java/com/springpractise/managementinformationsystem/controller/StudentController.java
@@ -1,23 +1,42 @@
 package com.springpractise.managementinformationsystem.controller;
 
 import com.springpractise.managementinformationsystem.entity.StudentDetails;
+import com.springpractise.managementinformationsystem.exception.CommonExceptionHandler;
 import com.springpractise.managementinformationsystem.service.StudentDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/studentdatabasemanagement")
+@RequestMapping("/v1/mis")
 public class StudentController {
 
     @Autowired
     private StudentDetailsService studentDetailsService;
 
     @GetMapping(value = "/studentdetails")
-    List<StudentDetails> retrieveStudentDetails() {
-        return  studentDetailsService.getStudentDetails();
+    public List<StudentDetails> getStudentDetails() {
+        return studentDetailsService.getStudentDetails();
+    }
+
+    @GetMapping(value = "/getstudentdetailsByStudentID")
+    public StudentDetails getStudentDetails(@RequestParam(required = false) Long StudentId) {
+        return studentDetailsService.getStudentByID(StudentId);
+    }
+
+    // Implementing the same as above using pathvariable
+    @GetMapping(value = "/getstudentdetailsByStudentID/{StudentId}")
+    public StudentDetails getStudentDetailss(@PathVariable Long StudentId) {
+        return studentDetailsService.getStudentByID(StudentId);
+    }
+
+    @PostMapping(value = "/newstudent")
+    public List<StudentDetails> createNewStudents(@RequestBody List<StudentDetails> newStudents) {
+        return studentDetailsService.createNewStudent(newStudents);
+
     }
 }
