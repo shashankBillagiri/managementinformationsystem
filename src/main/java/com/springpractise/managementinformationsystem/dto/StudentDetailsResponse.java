9b29690fd16bba5fa72450fad0140752bcae3f30
shashank <shashankbillagiri93@gmail.com>
2023-08-30 21:21:30 -0500

    Implemented pagination on Get Call.

---
+++ src/main/java/com/springpractise/managementinformationsystem/dto/StudentDetailsResponse.java
@@ -0,0 +1,21 @@
+package com.springpractise.managementinformationsystem.dto;
+
+import com.springpractise.managementinformationsystem.entity.StudentDetails;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDetailsResponse {
+
+    private List<StudentDetails> studentDetails;
+    private Integer pageSize;
+    private Integer pageNumber;
+    private Integer totalStudents;
+}
