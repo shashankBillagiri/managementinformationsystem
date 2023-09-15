c81d18bd9b677ffab9a9788796714f5da351c912
shashank <shashankbillagiri93@gmail.com>
2023-09-15 16:44:54 -0500

    refactored at few places.

---
+++ src/test/java/com/springpractise/managementinformationsystem/ManagementinformationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractise.managementinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementinformationsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
